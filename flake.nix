6ced7f603d13d8caa0b8578cae55a299e49ef42d
SpiralP <spiral.palmer@gmail.com>
2024-09-09 14:02:05 -0700

    nix: update hash

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
             cargoLock = {
               lockFile = ./Cargo.lock;
               outputHashes = {
-                "chatsounds-0.2.0" = "sha256-3LABx51hvXEMa6LWi65x7u3ELVHqrX80GD8kymOdwSg=";
+                "chatsounds-0.2.0" = "sha256-Nms5iWoB3rPPqouyzoQZTzIa7ni7bQAvkU1xbihQT5Y=";
               };
             };
 
